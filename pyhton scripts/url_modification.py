7e7bbc9135c808a3e2bb209183f0726a63104c00
MMPetrov20 <MMPetrov20@codingburgas.bg>
2023-07-05 03:06:53 +0300

    Implement page up function. Close #9

--- pyhton scripts/url_modification.py
+++ pyhton scripts/url_modification.py
@@ -50,3 +50,33 @@ def update_URL(str):
 
     # Return the modified URL
     return path
+
+# Point URL to the next product subcategory page
+def page_up(path, top_name_num):
+
+    if top_name_num != 1:
+        # Check if the URL already has a page number
+        if 'page' in path:
+            # Find the index of the last '/'
+            symbol_index = path.rfind("/")
+
+            if symbol_index != -1:
+                # Seperate the page number from the URL
+                page_num_str = path[(symbol_index + 1):]
+
+                # Get the page number as an integer
+                page_num_int = int(page_num_str)
+
+                # Update the page number
+                page_num_int += 1
+
+                # Replace the old page number with the new one
+                path = path.replace(page_num_str, str(page_num_int))
+        else:
+            # Update the URL with a page indicator
+            path += "/page/2"
+    else:
+        return path
+
+    # Return the updated URL
+    return path
