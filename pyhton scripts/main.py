6a510c609f50e58f85daa2474b37e60128dd465b
MMPetrov20 <MMPetrov20@codingburgas.bg>
2023-07-05 03:15:58 +0300

    Implement a continuous while loop. Close #11

--- pyhton scripts/main.py
+++ pyhton scripts/main.py
@@ -1,15 +1,40 @@
+import requests
+from bs4 import BeautifulSoup
 from menu import select_category
-from url_modification import update_URL
-from scraper import collect_data, print_data
+from url_modification import update_URL, page_up
+from scraper import collect_data, print_data, get_top_page_num
 
-# Get the target product category
-target_category = select_category()
+def main():
+    # Get the target product category
+    target_category = select_category()
 
-# Update the URL to point to the target product category
-URL = update_URL(target_category)
+    # Update the URL to point to the target product category
+    URL = update_URL(target_category)
 
-# Collect data from the URL
-collected_data = collect_data(URL, target_category)
+    # Collect data from the URL
+    page_data = []
+    page_num = 1
 
-# Print the collectted data
-print_data(collected_data)
+    while(True):
+        # Collect date from a page
+        page_data = collect_data(URL, target_category, page_num)
+
+        # Check if top page has been reached
+        if page_data != None:
+            # Print the data collected from the page
+            print_data(page_data, page_num)
+
+            if page_num != get_top_page_num():
+                # Update the URL to point to the next page
+                URL = page_up(URL, get_top_page_num())
+
+                # Update the page number
+                page_num += 1
+            else:
+                break
+        else:
+            break
+
+# Run main function
+if __name__ == "__main__":
+    main()
--- pyhton scripts/scraper.py
+++ pyhton scripts/scraper.py
@@ -6,13 +6,13 @@ specific_product_parameters = {
     'gaming PCs' : ['CPU', 'GPU', 'RAM', 'Storage', 'Warranty period'],
     'all-in-one PCs' : ['Monitor', 'CPU', 'GPU', 'RAM', 'Storage', 'Warranty period'],
     'mouses' : ['Connectivity', 'Sensor', 'Sensitivity', 'Number of buttons', 'Interface', 'Warranty period'],
-    'keyboards' : ['Technology', 'Interface', 'Backlight', 'BDS Cyrillic', 'Kit', 'Warranty'],
+    'keyboards' : ['Technology', 'Interface', 'Backlight', 'BDS Cyrillic', 'Warranty'],
     'monitors' : ['Size', 'Resolution', 'Matrix type', 'Viewing angle', 'Refresh rate', 'Brightness', 'Interfaces'],
     'gaming laptops' : ['CPU', 'GPU', 'RAM', 'Storage','Monitor', 'Refresh rate', 'Warranty period'],
     'portable laptops' : ['CPU', 'GPU', 'RAM', 'Storage','Monitor', 'Warranty period'],
     'apple smartphones' : ['Operating system', 'Display', 'Refresh rate', 'Chipset', 'RAM', 'Built-in memory', 'Camera', 'Warranty period'],
     'samsung smartphones' : ['Operating system', 'Display', 'Refresh rate', 'Chipset', 'RAM', 'Built-in memory', 'Camera', 'Warranty period'],
-    'xiaomi smartphones' : ['Operating system', 'Display', 'Refresh rate', 'Chipset', 'RAM', 'Built-in memory', 'Camera', 'Warranty period'],
+    'xiaomi smartphones' : ['Operating system', 'Display', 'Chipset', 'RAM', 'Built-in memory', 'Camera', 'Warranty period'],
     'asus smartphones' : ['Operating system', 'Display', 'Refresh rate', 'Chipset', 'RAM', 'Built-in memory', 'Camera', 'Warranty period'],
     'samsung TVs' : ['Screen size', 'Resolution', 'Warranty period'],
     'sony TVs' : ['Screen size', 'Matrix type', 'Resolution', 'Scan rate', 'Warranty period'],
@@ -45,54 +45,61 @@ def collect_data(URL, selected_category, page_num):
     global page, page_content
     page = requests.get(URL)
     page_content = BeautifulSoup(page.content, "html.parser")
-    products = page_content.find_all("div", class_="product")
-    product_info = []
 
-    # Iterate through the scraped products
-    for i in products:
-        # Build a list with the product's parameters
-        parameters = i.find("ul", class_="parameters")
-        
-        # Process the product's parameters
-        list_items = []
-        for li in parameters.find_all("li"):  
-            #  Cast the li(type: Any) to string
-            edited_li = str(li.text.strip())
+    # Check if the top page has been reached
+    if page_num <= get_top_page_num():
+        products = page_content.find_all("div", class_="product")
 
-            # Check for and edit odd list items
-            if(edited_li.find(':') > 0):
-                edited_li = edited_li.split(':', 1)[1]
-                edited_li = edited_li[1:]
+        # Define a list to holkd the product packages
+        product_info = []
 
-            # Append the processed item to the list
-            list_items.append(edited_li)
+        # Iterate through the scraped products
+        for i in products:
+            # Build a list with the product's parameters
+            parameters = i.find("ul", class_="parameters")
+            
+            # Process the product's parameters
+            list_items = []
+            for li in parameters.find_all("li"):  
+                #  Cast the li(type: Any) to string
+                edited_li = str(li.text.strip())
 
-        # Check if a product has enough listed parameters
-        if(len(list_items) == len(specific_product_parameters[selected_category])):
-            # Get the product's listed name
-            product_name = i.find("div", class_="isTruncated").text.strip()
+                # Check for and edit odd list items
+                if(edited_li.find(':') != -1):
+                    edited_li = edited_li.split(':', 1)[1]
+                    edited_li = edited_li[1:]
 
-            # Get the product's listed price
-            product_price = str(i.find("div", class_="prices").find("div", class_="price").find("span").text.strip())
-            product_price = product_price.replace("00", "") # Format out the extra zeros
+                # Append the processed item to the list
+                list_items.append(edited_li)
 
-            # Build a dictionary from the product parameters and their values 
-            parameter_package = dict(zip(specific_product_parameters[selected_category], list_items))
+            # Check if a product has enough listed parameters
+            if(len(list_items) == len(specific_product_parameters[selected_category])):
+                # Get the product's listed name
+                product_name = i.find("div", class_="isTruncated").text.strip()
 
-            # Join the dictionary of parameters with the product's name and price
-            product_package = {'Name': product_name, **parameter_package, 'Price' : product_price}
+                # Get the product's listed price
+                product_price = str(i.find("div", class_="prices").find("div", class_="price").find("span").text.strip())
+                edited_product_price = product_price[:-4] + product_price[-2:]
 
-            # Append the complete product package to the list
-            product_info.append(product_package)
+                # Build a dictionary from the product parameters and their values 
+                parameter_package = dict(zip(specific_product_parameters[selected_category], list_items))
 
-    # Return the finished list of product packages
-    return product_info
+                # Join the dictionary of parameters with the product's name and price
+                product_package = {'Name': product_name, **parameter_package, 'Price' : edited_product_price}
+
+                # Append the complete product package to the list
+                product_info.append(product_package)
+
+        # Return the finished list of product packages
+        return product_info
+    else:
+        return
 
 # Print collected data
-def print_data(product_info):
+def print_data(page_data, page_num):
 
     # Iterate throught the product
-    for dictionary in product_info:
+    for dictionary in page_data:
 
         # Print data from the product packages
         for key, value in dictionary.items():
